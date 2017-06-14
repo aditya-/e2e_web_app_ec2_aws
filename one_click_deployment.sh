557f3aee112c70d260e9d8f418b90c812ebad020
KARRI ADITYA <karri.aditya@tcs.com>
2017-06-15 01:41:10 +0530

    advanced vpc

--- one_click_deployment.sh
+++ one_click_deployment.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# created for automatic push
+# created for automatic deployement to Git and Server
 commit_name=$1
 
 if [[ -n "$commit_name" ]]; then
     git add .
     git commit -am "$1"
     git push origin master
+# Automatic code deployment to server    
 ssh ansi_aws << EOF
  cd /etc/ansible/roles/e2e_web_app_ec2_aws/ && sh pull.sh
  exit
